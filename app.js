d781c6a457fe89c9591befe0a03e08d49f7da85b
SUJAY GOWDA <sujaygowda.dev@gmail.com>
2025-05-14 12:07:31 +0530

    handling get request

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello from backend...');
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'Success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 const port = 3000;
