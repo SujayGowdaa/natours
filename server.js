9db4074f5d5fabd4492a291b4d7d8b3f65dc7da7
SUJAY GOWDA <sujaygowda.dev@gmail.com>
2025-05-18 11:55:38 +0530

    creating a document in DB by enforcing the mongoose model

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config({
   path: './config.env',
 });
-
 import app from './app.js';
 
 mongoose
@@ -12,8 +11,35 @@ mongoose
   .then(() => console.log('Database connected'))
   .catch((err) => console.log('Error 🔥:', err.errorResponse.errmsg));
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required!'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'Price is required!'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The test',
+  price: 200,
+});
+
+testTour
+  .save()
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
 // eslint-disable-next-line no-undef
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Running on port ${port}...`);
 });
