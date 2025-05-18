7dad60e0c2d3ad61fa9f3b806f0a2d619a4d60f5
SUJAY GOWDA <sujaygowda.dev@gmail.com>
2025-05-18 11:06:14 +0530

    connecting to database

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config({
   path: './config.env',
 });
 
 import app from './app.js';
+
+mongoose
+  // eslint-disable-next-line no-undef
+  .connect(process.env.DATABASE)
+  .then(() => console.log('Database connected'))
+  .catch((err) => console.log('Error 🔥:', err.errorResponse.errmsg));
 
 // eslint-disable-next-line no-undef
 const port = process.env.PORT;
