69988c8dddfc1baffb893866c31162088a4d1578
SUJAY GOWDA <sujaygowda.dev@gmail.com>
2025-05-18 15:10:30 +0530

    update document

--- controllers/tour.js
+++ controllers/tour.js
@@ -35,7 +35,6 @@ async function getTour(req, res) {
 async function createTour(req, res) {
   try {
     const newTour = await Tour.create(req.body);
-
     res.status(201).json({
       message: 'success',
       data: {
@@ -51,9 +50,24 @@ async function createTour(req, res) {
 }
 
 async function updateTour(req, res) {
-  res.status(200).json({
-    message: 'success',
-  });
+  try {
+    const updatedTour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true, // add the 3rd parameter to return the newly added document
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      message: 'success',
+      data: {
+        tour: updatedTour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'failed',
+      message: err,
+    });
+  }
 }
 
 async function deleteTour(req, res) {
