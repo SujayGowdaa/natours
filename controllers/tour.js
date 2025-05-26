8444615ef4f992ab845c3a246639148ecc3ac1c5
SUJAY GOWDA <sujaygowda.dev@gmail.com>
2025-05-26 11:56:26 +0530

    add pagination

--- controllers/tour.js
+++ controllers/tour.js
@@ -22,15 +22,30 @@ async function getAllTours(req, res) {
       query = query.select('-__v');
     }
 
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+
+      if (skip > numTours) {
+        throw new Error("This page doesn't exist.");
+      }
+    }
+
     const tours = await query;
 
     res.status(200).json({
       status: 'success',
+      results: tours.length,
       data: { tours },
     });
   } catch (err) {
     res.status(404).json({
-      status: 'failed',
+      status: 'fail',
       message: err,
     });
   }
@@ -47,7 +62,7 @@ async function getTour(req, res) {
     });
   } catch (err) {
     res.status(404).json({
-      status: 'failed',
+      status: 'fail',
       message: err,
     });
   }
@@ -64,7 +79,7 @@ async function createTour(req, res) {
     });
   } catch (err) {
     res.status(404).json({
-      status: 'failed',
+      status: 'fail',
       message: err,
     });
   }
@@ -85,7 +100,7 @@ async function updateTour(req, res) {
     });
   } catch (err) {
     res.status(404).json({
-      status: 'failed',
+      status: 'fail',
       message: err,
     });
   }
@@ -101,7 +116,7 @@ async function deleteTour(req, res) {
     });
   } catch (err) {
     res.status(404).json({
-      status: 'failed',
+      status: 'fail',
       message: err,
     });
   }
